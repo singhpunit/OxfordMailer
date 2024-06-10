8752d385f627231c886eb574b83eb6b3be508ea7
singhpunit <116079316+singhpunit@users.noreply.github.com>
2024-06-10 19:00:05 +0530

    Update server.js
    cros

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ const PORT = 6000;
 const mongoose = require("mongoose")
 const DB_URL = "mongodb://localhost:27017/mu2"
 // Middleware
+
 app.use(cors()); // Enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
