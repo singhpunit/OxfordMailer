0d0dc222c26362e8b92d3f359e2bdd96b80c8d62
singhpunit <116079316+singhpunit@users.noreply.github.com>
2024-06-12 16:02:15 +0530

    Update mailService.js
    email

--- controller/mailService.js
+++ controller/mailService.js
@@ -15,7 +15,7 @@ module.exports={
         });
 
         let mailDetails = {
-            from: 'chuohanshivam3@gmail.com',
+            from: 'connect@mu20.co',
             to: data.email,
             subject: "subject",
             text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
